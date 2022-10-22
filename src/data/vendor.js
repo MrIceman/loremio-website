fedca064253fb8ff62746ad2e8a6b77c780d9837
Martin Nowosad <mrtnnwsd@gmail.com>
2022-10-22 13:37:14 +0200

    updated prod link

--- src/data/vendor.js
+++ src/data/vendor.js
@@ -1,5 +1,5 @@
 export const getPush = async (pushId) => {
-    const resp = await fetch(`https://k42pcx1qi8.execute-api.eu-central-1.amazonaws.com/production/push/${pushId}`)
+    const resp = await fetch(`https://spddb7biyj.execute-api.eu-central-1.amazonaws.com/production/push/${pushId}`)
     const json = await resp.json()
     return json
 }
