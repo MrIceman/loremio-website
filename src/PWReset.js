ffb746b0c5653f87eab5927ec59445c4f87c2766
ammyt <a.m.tayea@gmail.com>
2022-10-25 21:40:22 +0200

    fix width

--- src/PWReset.js
+++ src/PWReset.js
@@ -12,59 +12,57 @@ const PWReset = (props) => {
   console.log(props);
 
   return (
-    <div className='password_reset__form'>
-      <div className='lock__icon'>
-        <LockOutlined style={{ fontSize: '72px' }} />
-      </div>
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <div style={{ margin: '30px 0', textAlign: 'center' }}>
-          Erstell ein neues Passwort und bestätige es um dein Passwort zu ändern.
+    <div>
+      <div className='password_reset__form content__body__element'>
+        <div className='lock__icon'>
+          <LockOutlined style={{ fontSize: '72px' }} />
         </div>
-
-        <Form.Item
-          hasFeedback
-          name="password"
-          rules={[{ required: true, message: 'Bitte Passwort eingeben' }]}
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input.Password prefix={<div style={{ marginRight: '5px' }}><UnlockOutlined className="site-form-item-icon" /></div>} placeholder="Passwort eingeben" />
-        </Form.Item>
-
-        <Form.Item
-          hasFeedback
-          name="repeat"
-          dependencies={['password']}
-          rules={[
-            {
-              required: true,
-              message: 'Bitte Passwort wiederholen',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error('Passwörter müssen identisch sein'));
-              },
-            }),
-          ]}
-        >
-          <Input.Password prefix={<div style={{ marginRight: '5px' }}><UnlockOutlined className="site-form-item-icon" /></div>} placeholder="Passwort widerholen" />
-        </Form.Item>
-
-        <div style={{ margin: '40px 0 0 0' }}>
-          <Form.Item>
-            <Button block type="primary" htmlType="submit" className="login-form-button">
-              Passwort ändern
-            </Button>
+          <div style={{ margin: '30px 0', textAlign: 'center' }}>
+            Erstell ein neues Passwort und bestätige es um dein Passwort zu ändern.
+          </div>
+          <Form.Item
+            hasFeedback
+            name="password"
+            rules={[{ required: true, message: 'Bitte Passwort eingeben' }]}
+          >
+            <Input.Password prefix={<div style={{ marginRight: '5px' }}><UnlockOutlined className="site-form-item-icon" /></div>} placeholder="Passwort eingeben" />
           </Form.Item>
-        </div>
-
-      </Form>
+          <Form.Item
+            hasFeedback
+            name="repeat"
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+                message: 'Bitte Passwort wiederholen',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('Passwörter müssen identisch sein'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password prefix={<div style={{ marginRight: '5px' }}><UnlockOutlined className="site-form-item-icon" /></div>} placeholder="Passwort widerholen" />
+          </Form.Item>
+          <div style={{ margin: '40px 0 0 0' }}>
+            <Form.Item>
+              <Button block type="primary" htmlType="submit" className="login-form-button">
+                Passwort ändern
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
